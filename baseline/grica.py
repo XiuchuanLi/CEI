fcd36f70017dc99f4fe2eb31e0ec76df6aa6acf1
XiuchuanLi <49651182+XiuchuanLi@users.noreply.github.com>
2025-09-19 17:29:16 +0400

    Add files via upload

---
+++ baseline/grica.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+import os
+from src.methods import graphical_rica
+from src.generate_data import generate_data
+from src.utils import performance
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+
+# hyper-parameters of RICA
+lr = 0.1
+w_init = 'cov_guess'
+lmbda_GRICA = 0
+momentum = 0
+epochs = 400
+samples_list = [500,1000,2000,5000,10000,15000,20000,25000]
+
+
+latent, observed = 1, 3
+for distribution in ['laplace', 'uniform']:
+    for graph in ['a', 'b', 'c', 'd', 'e']:
+        print(distribution, 'Fig. 3(', graph, ')')
+        results = [[], []] # mean, std
+        for n_samples in samples_list:
+            weight_GRICA_pred, weight_GRICA_true = [], []
+            weight_CM_pred, weight_CM_true = [], []
+            for seed in range(100):
+                data, g, weights, w_id = generate_data(graph, n_samples=n_samples, distribution=distribution, latent=latent, observed=observed, seed=seed)
+                #GRICA
+                d_cov = (data.t()).cov()
+                U, S, V = d_cov.svd()
+                S_2=torch.inverse(torch.diag(S.sqrt()))
+                W_w = S_2.matmul(V.t())
+                data_whitened = W_w.matmul(data.t()).t()
+                w_true = weights
+                loss_data, w_loss_data, weight_pred, weight_true = graphical_rica(latent, observed, g, data, data_whitened, epochs, lr, W_w, w_init, w_true, momentum, lmbda_GRICA)
+                tmp_pred, tmp_true = weight_pred[w_id].detach().item(), weight_true[w_id].detach().item()
+                weight_GRICA_pred.append(tmp_pred)
+                weight_GRICA_true.append(tmp_true)
+                
+            weight_GRICA_true = np.array(weight_GRICA_true)
+            weight_GRICA_pred = np.array(weight_GRICA_pred)
+            error_GRICA = np.abs(weight_GRICA_true - weight_GRICA_pred) / np.maximum(np.abs(weight_GRICA_true), np.abs(weight_GRICA_pred))
+            mean, std = performance(error_GRICA)
+            results[0].append(mean)
+            results[1].append(std)
+            print(f'{n_samples}: {mean:.2f}±{std:.2f}')
+        np.save(f'data/grica_{distribution}_{graph}.npy', results)
+
---
+++ readme.md
@@ -0,0 +1,48 @@
+All experiments are performed on an i9-13900F CPU. The total time cost is about 75 min.
+
+## Usage
+
+Install kerpy (a package adapted from https://github.com/oxcsml/kerpy)
+
+```(bash)
+cd kerpy
+python setup.py develop
+cd ..
+```
+
+Reproduce the results in Fig. 6.
+
+```(bash)
+cd algorithm
+python discrimination.py
+cd ..
+```
+
+Reproduce the results of our algorithm in Fig. 7.
+
+```(bash)
+cd algorithm
+python estimation.py
+cd ..
+```
+
+Reproduce the results of baselines (CM and GRICA) in Fig. 7.
+
+```(bash)
+cd baseline
+python cm.py
+python grica.py
+cd ..
+```
+
+## Requirements
+
+pytorch 2.1.2
+
+numpy 1.24.3
+
+scipy 1.10.1
+
+networkx 3.1
+
+matplotlib 3.7.1
